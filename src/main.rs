36423cbb91abf17d9da79e9a1cb8cd3a6592502d
Hendric Eckelt <Hendric.Eckelt@gmail.com>
2023-03-23 19:16:02 +0000

    Make the tool more generic
    Allow to pass in a schema file to fill arbitrary tables.

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,27 @@
 mod database;
-mod producers;
+mod table_schema;
 
 use dotenvy::dotenv;
-use producers::events;
-use std::env;
+use std::{env, fs};
+use table_schema::TableSchema;
 
 #[tokio::main]
 async fn main() -> Result<(), sqlx::Error> {
   dotenv().ok();
 
+  let schema_file = env::var("SCHEMA_FILE").expect("Failed to get SCHEMA_FILE environment variable");
+  let input = fs::read_to_string(&schema_file).expect(&format!("Failed to read {schema_file}"));
+  let schema: TableSchema = serde_json::from_str(&input).expect(&format!("Failed to parse {schema_file}"));
+
   let delimiter = env::var("DELIMITER").expect("Failed to get DELIMITER environment variable");
   let quantity = str::parse::<i32>(&env::var("QUANTITY").expect("Failed to get QUANTITY environment variable"))
     .expect("QUANTITY environment variable is not a number");
-  let table = env::var("TABLE").expect("Failed to get TABLE environment variable");
 
   let mut connection = database::connect().await?;
-  let statement = format!("COPY {table} FROM STDIN WITH (DELIMITER '{delimiter}', NULL 'NULL')");
+  let statement = format!("COPY {} FROM STDIN WITH (DELIMITER '{delimiter}', NULL 'NULL')", schema.name);
   let mut stream = connection.copy_in_raw(&statement).await?;
-  events::generate(&mut stream, quantity).await?;
+
+  schema.generate(&mut stream, quantity).await?;
   stream.finish().await?;
 
   Ok(())
---
+++ src/table_schema.rs
@@ -0,0 +1,53 @@
+use serde::Deserialize;
+use sqlx::postgres::{PgConnection, PgCopyIn};
+use std::env;
+use uuid::Uuid;
+
+#[derive(Deserialize, Debug)]
+pub struct TableSchema {
+  pub name: String,
+  pub columns: Vec<Column>,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Column {
+  #[serde(rename = "type")]
+  pub kind: Option<String>,
+  pub value: Option<String>,
+}
+
+impl TableSchema {
+  pub async fn generate(&self, stream: &mut PgCopyIn<&mut PgConnection>, quantity: i32) -> Result<(), sqlx::Error> {
+    let delimiter = env::var("DELIMITER").expect("Failed to get DELIMITER environment variable");
+
+    for _ in 1..=quantity {
+      stream.send(self.to_csv(&delimiter).as_bytes()).await?;
+      stream.send("\n".as_bytes()).await?;
+    }
+
+    Ok(())
+  }
+
+  pub fn to_csv(&self, delimiter: &str) -> String {
+    self
+      .columns
+      .iter()
+      .map(|column| {
+        if column.value.is_some() {
+          column.value.clone().unwrap()
+        } else {
+          self.generate_from_kind(&column.kind.clone().unwrap())
+        }
+      })
+      .collect::<Vec<String>>()
+      .join(delimiter)
+  }
+
+  fn generate_from_kind(&self, kind: &str) -> String {
+    match kind {
+      "timestamp" => String::from("now()"),
+      "uuid" => Uuid::new_v4().to_string(),
+      _ => String::from(""),
+    }
+  }
+}
